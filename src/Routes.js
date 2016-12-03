99707cfa8be4730bab746070c5a49022ef15a75e
Supanut Apikulvanich <supanut.overload@gmail.com>
2016-12-03 17:01:28 +0700

    Updated room page

--- src/Routes.js
+++ src/Routes.js
@@ -2,12 +2,13 @@ import React from 'react';
 import { Router, Route, IndexRedirect, browserHistory } from 'react-router'
 import App from './App';
 import List from './List';
+import Room from './Room';
 
 const Routes = () => (
   <Router history={browserHistory}>
     <Route path="/" component={App}/>
     <Route path="/list" component={List}/>
-    <Route path="/detail" component={List}/>
+    <Route path="/room" component={Room}/>
     <Route path="/map" component={List}/>
   </Router>
 );
