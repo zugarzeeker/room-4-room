be25091d1220e2ebed4ab37370c7c3a28d119842
Supanut Apikulvanich <supanut.overload@gmail.com>
2016-12-03 19:33:27 +0700

    Icon night

--- src/Item.js
+++ src/Item.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Item.scss';
 import { Link } from 'react-router';
+import _ from 'lodash';
 
 const Item = ({ item, data }) => (
   <div className="row">
@@ -11,7 +12,12 @@ const Item = ({ item, data }) => (
       </div>
       <div className="col-md-5">
           <h3>Room {data}</h3>
-          <h4>{data} night</h4>
+          {/*<h4>{data} night</h4>*/}
+          {/*<h4>{data} night</h4>*/}
+          {/*<h4>{data} night</h4>*/}
+          {_.range(data).map(() => (
+            <i className="fa fa-moon-o night-icon"></i>
+          ))}
           <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ut, odit velit cumque vero doloremque repellendus distinctio maiores rem expedita a nam vitae modi quidem similique ducimus! Velit, esse totam tempore.</p>
           <Link className="btn btn-primary" href="/room">View Detail <span className="glyphicon glyphicon-chevron-right"></span></Link>
       </div>
