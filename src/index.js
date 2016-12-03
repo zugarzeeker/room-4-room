7372591702797880484fcd6067c6ebe389085fe3
Supanut Apikulvanich <supanut.overload@gmail.com>
2016-12-03 14:45:56 +0700

    Added react router

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import Routes from './Routes';
 import './index.scss';
 
 ReactDOM.render(
-  <App />,
+  <Routes/>,
   document.getElementById('root')
 );
